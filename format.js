929a99dd558b2f0c69e19285adb91a820216c843
sWgxPaperfish <64800072+sWgxPaperfish@users.noreply.github.com>
2020-05-10 01:11:10 +0800

    Add files via upload

--- format.js
+++ format.js
@@ -18,10 +18,8 @@ function varifybutton2(){
       var datainstring = text;
       // console.log(data);
   });
-console.log(data);
   var varifytime = 0;
   var varifyinggg = window.setInterval(()=>{
-
     document.getElementById("page5-button").innerHTML ="Verify (" + varifytime + "s)";
     if (varifytime>0){varifytime = varifytime-1;document.getElementById("page5-password").innerHTML = "Please wait for the cool-down time"}
     else if (varifytime<=0){
@@ -29,13 +27,19 @@ console.log(data);
       document.getElementById("page5-button").addEventListener(
       "click",()=>{varifytime=5;
         var theusernameinput = document.getElementById("usernameInput").value;
-        if( document.getElementById("passwordInput").value == data[theusernameinput] )
-        {alert("It seems that you know how to read html and javascript!\nWell done!");
-        clearInterval(varifyinggg);
-      document.getElementById("usernameInput").value = "";
-      document.getElementById("passwordInput").value = "";
-      document.getElementById("page5-password").innerHTML = "You are the Genius Webpage Developer!"}
-        else{document.getElementById("page5-password").innerHTML = "Invalid Username or Password";}
-      })
+        readTextFile("../Project/password.json", function(text){
+            var data = JSON.parse(text);
+            var datainstring = text;
+            // console.log(data);
+            if( document.getElementById("passwordInput").value == data[theusernameinput] )
+            {alert("It seems that you know how to read html and javascript!\nWell done!");
+            clearInterval(varifyinggg);
+          document.getElementById("usernameInput").value = "";
+          document.getElementById("passwordInput").value = "";
+          document.getElementById("page5-password").innerHTML = "You are the Genius Webpage Developer!"}
+            else{document.getElementById("page5-password").innerHTML = "Invalid Username or Password";}
+          })
+        });
+
     }
 },1000);}
